0ec41268e4a61d3bf19dce68bdccff764f9beeb7
Tiseno <math.lindgren@gmail.com>
2022-12-04 12:42:59 +0100

    Fixed incorrect naming.

--- day04/main.lua
+++ day04/main.lua
@@ -7,7 +7,7 @@ local function assignment_str_to_set(assignment_str)
     return set
 end
 
-local function assignments_intersects(assignment_pair)
+local function assignment_pair_is_invalid(assignment_pair)
     local sets = lh.map(assignment_str_to_set, assignment_pair)
     return lh.is_superset(sets[1], sets[2]) or lh.is_superset(sets[2], sets[1])
 end
@@ -15,17 +15,17 @@ end
 local function solution1(filename)
     local list = lh.read_list(filename)
     local assignment_string_pairs = lh.map(lh.split_string_sep(","), list)
-    local intersections =
-        lh.map(assignments_intersects, assignment_string_pairs)
-    local number_of_intersections = lh.count(function(e) return e == true end,
-                                             intersections)
-    lh.P(number_of_intersections)
+    local intersections = lh.map(assignment_pair_is_invalid,
+                                 assignment_string_pairs)
+    local number_of_invalid = lh.count(function(e) return e == true end,
+                                       intersections)
+    lh.P(number_of_invalid)
 end
 
 solution1("input.txt")
 solution1("input2.txt")
 
-local function assignments_overlaps(assignment_pair)
+local function assignments_intersects(assignment_pair)
     local sets = lh.map(assignment_str_to_set, assignment_pair)
     local intersection = lh.intersection(sets[1], sets[2])
     return lh.keys_in_table(intersection) > 0
@@ -34,7 +34,8 @@ end
 local function solution2(filename)
     local list = lh.read_list(filename)
     local assignment_string_pairs = lh.map(lh.split_string_sep(","), list)
-    local intersections = lh.map(assignments_overlaps, assignment_string_pairs)
+    local intersections =
+        lh.map(assignments_intersects, assignment_string_pairs)
     local number_of_intersections = lh.count(function(e) return e == true end,
                                              intersections)
     lh.P(number_of_intersections)
