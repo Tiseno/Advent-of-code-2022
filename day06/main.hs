21fd3beb618d28490ac75d44338523abdf50a0b2
Tiseno <math.lindgren@gmail.com>
2022-12-06 10:12:59 +0100

    Changed to use guards instead of if then else.

--- day06/main.hs
+++ day06/main.hs
@@ -4,13 +4,12 @@ hasDoubles (x:xs) = elem x xs || hasDoubles xs
 
 slidingR :: Int -> Int -> String -> String -> Int
 slidingR windowSize i [] _ = 0
-slidingR windowSize i (x:xs) window =
-        if length (window ++ [x]) < windowSize
-        then slidingR windowSize (i+1) xs (window ++ [x])
-        else
-            if hasDoubles (window ++ [x])
-            then slidingR windowSize (i+1) xs (tail window ++ [x])
-            else i
+slidingR windowSize i (x : xs) window
+    | length (window ++ [x]) < windowSize
+    = slidingR windowSize (i + 1) xs (window ++ [x])
+    | hasDoubles (window ++ [x])
+    = slidingR windowSize (i + 1) xs (tail window ++ [x])
+    | otherwise = i
 
 solution1 s = slidingR 4 1 s []
 solution2 s = slidingR 14 1 s []
