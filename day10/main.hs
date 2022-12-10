8dd68c1816a9095600ba975df271d78adc97fe2c
Tiseno <math.lindgren@gmail.com>
2022-12-10 23:25:50 +0100

    Day 10 tiny tiny improvement and small clean up.

--- day10/main.hs
+++ day10/main.hs
@@ -1,26 +1,21 @@
-import Data.Maybe
-
 data Instruction = Noop | Addx Int
-  deriving Show
 
 parseInstruction :: String -> Instruction
 parseInstruction "noop" = Noop
 parseInstruction instr = Addx $ read $ last $ words instr
 
 executionTime :: Instruction -> Int
-executionTime (Addx _) = 1
 executionTime Noop = 0
+executionTime (Addx _) = 1
 
 runInstruction Noop register = register
 runInstruction (Addx i) register = register + i
 
-drawPixel image x = let drawX = length image `mod` 40 in image ++ [if x < drawX + 2 && x > drawX - 2 then '#' else ' ']
+drawPixel image x = image ++ [if 2 > abs (length image `mod` 40 - x) then '#' else ' ']
 
 type Counter = Int
-type Image = String
-type Register = Int
+type CRTState = (String, Int)
 type CpuState = (Counter, Instruction, [Instruction])
-type CRTState = (Image, Register)
 
 runCycles :: Counter -> CRTState -> CpuState -> CRTState
 runCycles 0 state (_, _, _) = state
@@ -32,17 +27,14 @@ runCycles c (image, x) (0, instr, instructions)  =
 runCycles c (image, x) (ic, instr, instructions)  =
   runCycles (c - 1) (drawPixel image x, x) (ic - 1, instr, instructions)
 
-xAtCycle instructions n = snd $ runCycles n ("", 1) (0, Noop, instructions)
-
+-- Part 1
 signalStrengthAtCycle instructions n = (*n) $ snd (runCycles n ("", 1) (0, Noop, instructions))
-
 sumOfSixSignalStrengths instructions = sum $ fmap (signalStrengthAtCycle instructions) [20, 60, 100, 140, 180, 220]
 
+-- Part 2
 createImage instructions = fst $ runCycles (-1) ("", 1) (0, Noop, instructions)
-
 chunksOf _ [] = []
 chunksOf n xs = let (ys, zs) = splitAt n xs in ys : chunksOf n zs
-
 drawImage image = mapM_ putStrLn (chunksOf 40 image)
 
 main = do
