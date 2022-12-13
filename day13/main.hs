07be723b34c2ded687a4c45faf59f18fcd3b1abc
Tiseno <math.lindgren@gmail.com>
2022-12-13 21:57:03 +0100

    Day 13 part 2.

--- day13/main.hs
+++ day13/main.hs
@@ -1,42 +1,53 @@
 import Data.Char
+import Data.List
 import Data.List.Split
 
 data Packet = DataList [Packet] | Data Int
+
 instance Show Packet where
   show (DataList a) = show a
   show (Data a) = show a
 
-parseDataInt :: String -> (Packet, String)
-parseDataInt s = let (i, remaining) = head $ (reads :: ReadS Int) s in (Data i, remaining)
+instance Eq Packet where
+  (==) (Data n) (Data m) = n == m
+  (==) (DataList a) (DataList b) = a == b
+  (==) _ _ = False
+
+instance Ord Packet where
+  compare (Data n) (Data m) = compare n m
+  compare (DataList a) (DataList b) =
+    let comp = foldl (\r (ae, be) -> if r /= EQ then r else compare ae be) EQ (zip a b) in
+    if comp /= EQ then comp else compare (length a) (length b)
+  compare d@(Data n) dataList = compare (DataList [d]) dataList
+  compare dataList d@(Data n)  = compare dataList (DataList [d])
+
+parseData :: String -> (Packet, String)
+parseData s = let (i, remaining) = head $ (reads :: ReadS Int) s in (Data i, remaining)
 
 parseDataList :: [Packet] -> String -> ([Packet], String)
 parseDataList p (']':xs) = (p, xs)
 parseDataList p (',':xs) = parseDataList p xs
-parseDataList p s@(x:xs) = let (packet, remaining) = parseDataR s in parseDataList (p ++ [packet]) remaining
+parseDataList p s@(x:xs) = let (packet, remaining) = parsePacketR s in parseDataList (p ++ [packet]) remaining
 
-parseDataR :: String -> (Packet, String)
-parseDataR s@('[':xs) = let (packets, remaining) = parseDataList [] xs in (DataList packets, remaining)
-parseDataR s = parseDataInt s
+parsePacketR :: String -> (Packet, String)
+parsePacketR s@('[':xs) = let (packets, remaining) = parseDataList [] xs in (DataList packets, remaining)
+parsePacketR s = parseData s
 
-parseData :: String -> Packet
-parseData s = fst $ parseDataR s
+parsePacket :: String -> Packet
+parsePacket s = fst $ parsePacketR s
 
-listPair [a, b] = (a, b)
-parseInput input = fmap (listPair . fmap parseData) (splitWhen (== "") (lines input))
+parsePacketPairs input = fmap ((\[a, b] -> (a, b)) . fmap parsePacket) (splitWhen (== "") (lines input))
+parsePackets input = parsePacket <$> filter (/="") (lines input)
 
-compareData :: Packet -> Packet -> Ordering
-compareData (Data n) (Data m) = compare n m
-compareData (DataList a) (DataList b) =
-  let comp = foldl (\r (ae, be) -> if r /= EQ then r else compareData ae be) EQ (zip a b) in
-  if comp /= EQ then comp else compare (length a) (length b)
-compareData d@(Data n) dataList = compareData (DataList [d]) dataList
-compareData dataList d@(Data n)  = compareData dataList (DataList [d])
-
-part1 packetPairs = sum $ fmap fst $ filter (\(_, ord) -> ord == LT) $ zip [1..] $ fmap (uncurry compareData) packetPairs
+part1 packetPairs = sum $ fmap fst $ filter (\(_, ord) -> ord == LT) $ zip [1..] $ fmap (uncurry compare) packetPairs
+part2 packets = let extraPackets = [parsePacket "[[2]]", parsePacket "[[6]]]"] in
+  product $ fmap fst $ filter (\(_, p) -> p `elem` extraPackets) $ zip ([1..]::[Int]) $ sort (packets ++ extraPackets)
 
 main = do
-  packetPairsE1 <- parseInput <$> readFile "input_e1.txt"
-  print $ part1 packetPairsE1
-  packetPairs <- parseInput <$> readFile "input.txt"
-  print $ part1 packetPairs
+  inputE1 <- readFile "input_e1.txt"
+  input <- readFile "input.txt"
+  print $ part1 $ parsePacketPairs inputE1
+  print $ part1 $ parsePacketPairs input
+  print $ part2 $ parsePackets inputE1
+  print $ part2 $ parsePackets input
 
