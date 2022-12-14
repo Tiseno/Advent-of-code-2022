cc95e5e860a9603454a3d57613e31da8011a9f0e
Tiseno <math.lindgren@gmail.com>
2022-12-15 00:17:08 +0100

    Refactored distance function.

--- day12/main.hs
+++ day12/main.hs
@@ -17,8 +17,9 @@ charToHeight c = let c2 | c == 'S' = 'a' | c == 'E' = 'z' | otherwise = c in ord
 initialDistance :: Char -> Char -> Int
 initialDistance start c | c == start = 0 | otherwise = 100000000000000
 
-type DistFunc = Matrix Int -> Point -> Point -> Int
-cliffDistance :: DistFunc
+type DistFunc = Point -> Point -> Int
+
+cliffDistance :: Matrix Int -> DistFunc
 cliffDistance heightMap from to =
   let fromCliff = uncurry getElem from heightMap in
   let toCliff = uncurry getElem to heightMap in
@@ -30,7 +31,7 @@ prioInsert :: PrioPoint -> [PrioPoint] -> [PrioPoint]
 prioInsert p [] = [p]
 prioInsert p (x:xs) = if fst p < fst x then p:x:xs else x : prioInsert p xs
 
-type DState = (Matrix Int, Matrix Point, [PrioPoint], Set.Set Point, Matrix Int)
+type DState = (Matrix Int, Matrix Point, [PrioPoint], Set.Set Point)
 
 unvisitedNeighborsOf :: Set.Set Point -> Int -> Int -> Point -> [Point]
 unvisitedNeighborsOf visited xmax ymax (x, y) =
@@ -39,19 +40,19 @@ unvisitedNeighborsOf visited xmax ymax (x, y) =
   Data.List.filter (`notElem` visited) inBounds
 
 visitNeighbor :: DistFunc -> Point -> DState -> Point -> DState
-visitNeighbor distFunc u dstate@(dist, path, queue, visited, heightMap) n =
-  let newDistance = uncurry getElem u dist + distFunc heightMap u n in
+visitNeighbor distFunc u dstate@(dist, path, queue, visited) n =
+  let newDistance = uncurry getElem u dist + distFunc u n in
   if newDistance >= uncurry getElem n dist then dstate else
-  (setElem newDistance n dist, setElem u n path, prioInsert (newDistance, n) queue, visited, heightMap)
+  (setElem newDistance n dist, setElem u n path, prioInsert (newDistance, n) queue, visited)
 
 shortestPathsR :: DistFunc -> DState -> DState
-shortestPathsR distFunc dstate@(_, _, [], _, _) = dstate
-shortestPathsR distFunc dstate@(dist, path, queue, visited, heightMap) =
+shortestPathsR distFunc dstate@(_, _, [], _) = dstate
+shortestPathsR distFunc dstate@(dist, path, queue, visited) =
   let (_, u):queue2 = queue in
-  if u `elem` visited then shortestPathsR distFunc (dist, path, queue2, visited, heightMap) else
+  if u `elem` visited then shortestPathsR distFunc (dist, path, queue2, visited) else
   let visited2 = Set.insert u visited in
   let ns = unvisitedNeighborsOf visited (nrows dist) (ncols dist) u in
-  shortestPathsR distFunc $ Data.List.foldl (visitNeighbor distFunc u) (dist, path, queue2, visited2, heightMap) ns
+  shortestPathsR distFunc $ Data.List.foldl (visitNeighbor distFunc u) (dist, path, queue2, visited2) ns
 
 part1 chars =
   let charMatrix = fromLists chars in
@@ -60,7 +61,7 @@ part1 chars =
   let paths = mapPos const distances in
   let start = elemPoint 'S' chars in
   let end = elemPoint 'E' chars in
-  let (d, _, _, _, _) = shortestPathsR cliffDistance (distances, paths, [(0, start)], Set.empty, heightMap) in
+  let (d, _, _, _) = shortestPathsR (cliffDistance heightMap) (distances, paths, [(0, start)], Set.empty) in
   uncurry getElem end d
 
 fmapi :: (Int -> a -> b) -> [a] -> [b]
@@ -69,11 +70,11 @@ fmapi f list = snd $ Data.List.foldl (\(i, b) a -> (i+1, b ++ [f i a])) (0, []) 
 elemPoints :: Eq a => a -> [[a]] -> [Point]
 elemPoints a m = fmap fst $ Data.List.filter (\(p, c) -> c == a) $ concat $ fmapi (\i row -> fmapi (\j e -> ((i + 1, j + 1), e)) row) m
 
-cliffDistanceReverse :: DistFunc
+cliffDistanceReverse :: Matrix Int -> DistFunc
 cliffDistanceReverse heightMap from to =
   let fromCliff = uncurry getElem from heightMap in
   let toCliff = uncurry getElem to heightMap in
-  if toCliff + 1< fromCliff then 100000000000000 else 1
+  if toCliff + 1 < fromCliff then 100000000000000 else 1
 
 part2 chars =
   let charMatrix = fromLists chars in
@@ -83,7 +84,7 @@ part2 chars =
   let start = elemPoint 'S' chars in
   let end = elemPoint 'E' chars in
   let aPoints = elemPoints 'a' chars in
-  let (d, _, _, _, _) = shortestPathsR cliffDistanceReverse (distances, paths, [(0, end)], Set.empty, heightMap) in
+  let (d, _, _, _) = shortestPathsR (cliffDistanceReverse heightMap) (distances, paths, [(0, end)], Set.empty) in
   let aDistances = sort $ fmap (flip (uncurry getElem) d) aPoints in
   head aDistances
 
